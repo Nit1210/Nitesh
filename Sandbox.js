f2476841bad0ef9147c2558e0b4bb73fa1a333d1
Nitesh Kalyan Koilada <98055206+Nit1210@users.noreply.github.com>
2023-11-03 18:07:03 -0400

    Javascript score code was added

---
+++ Sandbox.js
@@ -0,0 +1,18 @@
+const correctAnswers=['C','B','A','B','B'];
+const form=document.querySelector('quiz-form');
+
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+
+    let score=0;
+    //getting user answers
+    const userAnswers=[form.q1.value,form.q2.value,form.q3.value,form.q4.value,form.q5.value];
+
+    //check the score
+    userAnswers.forEach((answers,index)=>{
+        if(answers=== correctAnswers[index]){
+            score +=25;
+        }
+        console.log(score);
+    })
+});
