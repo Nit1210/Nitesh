fc0fbae047379f11badc81782a26ad11fdd5759f
Nitesh Kalyan Koilada <98055206+Nit1210@users.noreply.github.com>
2023-11-07 18:57:13 +0000

    improved code structure

--- Sandbox.js
+++ Sandbox.js
@@ -1,45 +1,42 @@
-const correctAnswers=['C','B','A','B','B'];
-const form=document.querySelector('.quiz-form');
-const result=document.querySelector('.result');
-let score=0;
-form.addEventListener( 'submit', e =>{
+const correctAnswers = ['C', 'B', 'A', 'B', 'B'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+const submitButton = document.getElementById('submitbutton');
+let score = 0;
+
+form.addEventListener('submit', e => {
     e.preventDefault();
+    if (confirm("Are you sure you want to submit?")) {
+        // Disabling the submit button
+        submitButton.disabled = true;
 
-   alert("Are sure we want to submit?");
-    //getting user answers
-    const userAnswers=[form.q1.value,form.q2.value,form.q3.value,form.q4.value,form.q5.value];
+        // Getting user answers
+        const userAnswers = [
+            form.q1.value,
+            form.q2.value,
+            form.q3.value,
+            form.q4.value,
+            form.q5.value
+        ];
 
-    //check the score
-    userAnswers.forEach((answers,index)=>{
-        if(answers=== correctAnswers[index]){
-            score +=20;
-        }
-        if(answers[index]!=null){
-            
-            document.getElementById('sumbitbutton').disabled=true;
-           }
-       
-       // console.log(score);
-    })
-    
-   
-    scrollTo(0,0);
-    result.className="result py-4  bg-light text-center";
-    let output=0;
-    const timmer=setInterval(() =>{
-        result.getElementsByTagName("span")[0].innerText=`${output}%`
-       
-        if(output===score){
-            clearInterval;
-        }
-        else{
-            output++;
-        }
-    },10);
-    // console.log(result.getElementsByTagName("span")[0].innerText=`${score}%`);
-   //disable Submit Button if score is > 20
-  
-   
+        // Check the score
+        userAnswers.forEach((answer, index) => {
+            if (answer === correctAnswers[index]) {
+                score += 20;
+            }
+        });
 
+        // Displaying the score gradually
+        scrollTo(0, 0);
+        result.className = 'result py-4 bg-light text-center';
+        let output = 0;
+        const timer = setInterval(() => {
+            result.getElementsByTagName('span')[0].innerText = `${output}%`;
+            if (output === score) {
+                clearInterval(timer);
+            } else {
+                output++;
+            }
+        }, 10);
+    }
 });
-
